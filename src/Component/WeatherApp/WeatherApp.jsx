1dd70f96579adb3780216b26bfb50491574e0dc7
Sachin Nazare <sachinnazare58@gmail.com>
2023-09-23 18:59:28 +0530

    search_function

--- src/Component/WeatherApp/WeatherApp.jsx
+++ src/Component/WeatherApp/WeatherApp.jsx
@@ -8,14 +8,21 @@ import wind_icon from '../Assets/wind.png'
 import humidity_icon from '../Assets/humidity.png'
 
 const WeatherApp = () => {
+    let api_key= "8fbd24ed860342e10abd3367b08e1c76";
     var search_icon=<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-search" viewBox="0 0 16 16">
     <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z"/>
   </svg>
+
+  const serach = () => {
+        const element = document.getElementsByClassName("cityInput")
+        if(element[0].value==="")
+        {return 0;}
+  } 
   return (
     <div className='container'>
         <div className="top-bar">
             <input type="text" className="cityInput" />
-            <div className="search-icon">
+            <div className="search-icon" onClick={()=>{serach()}}>
                 {search_icon}
             </div>
         </div>
@@ -24,6 +31,7 @@ const WeatherApp = () => {
         </div>
         <div className="weather-temp">  24 C</div>
         <div className="weather-location"> London</div>
+        
         <div className="data-container">
             <div className="element">
                 <img src={humidity_icon} alt="" className="icon" />
@@ -36,7 +44,7 @@ const WeatherApp = () => {
                 <img src={wind_icon} alt="" className="icon" />
                 <div className="data">
                     <div className="humidity-percentage"> 18 km/hr</div>
-                    <div className="text-type"> Wind speed </div>
+                    <div className="text-type"> Wind Speed </div>
                 </div>
             </div>
         </div>
